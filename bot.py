e4fc6259e0acc2ed7c6c88a39356c0a46ef53d1c
sonny sheridan <sonny.sheridan@yahoo.co.uk>
2021-01-06 22:04:38 +0000

    Built on reply method, need to add a already_replied file

--- bot.py
+++ bot.py
@@ -17,7 +17,9 @@ class Bot(pw.Reddit):
     #only enter lower case
     keyWords = ['london', 'g', 'fam', 'inabit', 'sn',
                 'lewisham', 'safe', '420', 'wag1',
-                'wagwan']
+                'wagwan', 'catford', 'ladywell',
+                'brockley', 'croften', '69', 'bruv',
+                'brudda']
     
     def __init__(self,
                  client_id,
@@ -59,18 +61,18 @@ class Bot(pw.Reddit):
             
             comments = submit.comments
             matches = []
-            commentRange = numCom
-
-            
+                        
             for word in Bot.keyWords:
-                for i in range(commentRange):
+                for i in range(numCom):
                     try:
                         if word in comments[i].body.lower():
-                            print('Matched comment')
                             comment = comments[i]
                             info = [comment.id, comment.author, word]
                             matches.append(info)
-                    
+                            print('Matched comment')
+                        else:
+                            print('Comment not matched')
+                            
                     except IndexError as e:
                         print(e)
                         break
@@ -78,10 +80,38 @@ class Bot(pw.Reddit):
                          
         return matches
 
-    def sendComments(matches):
-        ''' will send a comment to the matches ''' 
+    def sendComments(self, matches):
+        ''' will send a comment to the matches '''
 
+        #make this a file that gets read chapter 7
+        replied_to = []
 
+        for match in matches:
+            comId = match[0]
+            comAuthor = str(match[1])
+            matchedWord = str(match[2])
+
+            if comId in replied_to:
+                continue
+            
+            elif matchedWord == '420':
+                reply = '420 nice'
+                
+            elif matchedWord == '69':
+                reply = '69 nice'
+                
+            else:    
+                reply = 'Yo ' + comAuthor + ' I noticed you said ' + matchedWord
+                reply += '. I know the chances are slim but if by any chance'
+                reply += ' you\'re from SE London, I just want to take this opertunity to say:'
+                reply += ' hope you have a great day, much love my g!!'
+                reply += '\n\nThis is a bot btw'
+
+            comment = self.comment(comId)
+            comment.reply(reply)
+            comment.upvote()
+            replied_to.append(comId)
+         
 #initalsing instance of Reddit class
 bot = Bot(
     client_id = os.getenv("client_id"),
@@ -90,11 +120,15 @@ bot = Bot(
     username = os.getenv("redditUsername"),
     password = os.getenv("redditPassword"))
 
-matches = bot.lookForKeyWords('coding', numPost = 10, numCom = 20)
-matches.append(bot.lookForKeyWords('learnProgramming', numPost = 10, numCom = 20))
-matches.append(bot.lookForKeyWords('python', numPost = 10, numCom = 20))
-matches.append(bot.lookForKeyWords('developer', numPost = 10, numCom = 20))
+matches = bot.lookForKeyWords('coding', numPost = 5, numCom = 10)
+#matches.append(bot.lookForKeyWords('learnProgramming', numPost = 10, numCom = 20))
+#matches.append(bot.lookForKeyWords('python', numPost = 10, numCom = 20))
+#matches.append(bot.lookForKeyWords('developer', numPost = 10, numCom = 20))
 
 print('\n\nNumber of matches',str(len(matches)))
+for match in matches:
+    print(match)
+
+bot.sendComments(matches)
 
 input('Enter to exit')
